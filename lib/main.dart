a0d4085d331d4da5356254ed57d9127c2a65b567
Ibby360 <ibrahimramadhan360@outlook.com>
2021-10-10 23:54:10 +0300

    Navigate to the recipe detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'models/recipe.dart';
+import 'screens/recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -54,8 +55,14 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
           itemCount: Recipe.samples.length,
           itemBuilder: (BuildContext context, int index) {
-            // TODO: Add GestureDetector
-            return buildRecipeCard(Recipe.samples[index]);
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return buildRecipeCard(Recipe.samples[index]);
+                }));
+              },
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
           },
         ),
       ),
